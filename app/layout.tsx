637b5028bb6f33fbe4b795b1aaa66cd5361ab737
kimmimok <kjh@hyojachoe.goe.go.kr>
2025-09-08 15:41:03 +0900

    2025-09-08 15:41

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             
             <div className="p-4 border-t border-gray-200">
               <div className="text-xs text-gray-500">
-                © 2025 Restaurant POS
+                행복한 하루 되세요!
               </div>
             </div>
           </nav>
